24f1914374211bcfdd1f2521e896d7050b4da691
ashdkx <ashdkx@yahoo.com>
2021-10-20 22:52:58 -0400

    created Compensation and ReportingStructure model

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,40 @@
+package com.mindex.challenge.data;
+
+
+import java.sql.Date;
+
+public class Compensation {
+    Employee employee;
+    int salary;
+    Date effectiveDate;
+
+    public Compensation(Employee employee, int salary, Date effectiveDate) {
+        this.employee = employee;
+        this.salary = salary;
+        this.effectiveDate = effectiveDate;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Date getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(Date effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+}
