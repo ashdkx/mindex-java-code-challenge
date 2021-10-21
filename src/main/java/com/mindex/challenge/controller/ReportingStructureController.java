99479c02a7c827aef7e963d46f3b9dcf227f4adc
ashdkx <ashdkx@yahoo.com>
2021-10-20 23:25:16 -0400

    init ServiceImpl classes and fixed annotations

--- src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ReportingStructureController {
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 
---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,13 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+    @Override
+    public ReportingStructure read(String id) {
+        return null;
+    }
+}
