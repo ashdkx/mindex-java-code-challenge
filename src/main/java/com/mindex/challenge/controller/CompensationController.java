aa0f7452dd4e861cb28c9f3bfa76d236b2ca23d5
ashdkx <ashdkx@yahoo.com>
2021-10-25 16:57:36 -0400

    change return type of controller to ResponseEntity and handle errors if thrown

--- src/main/java/com/mindex/challenge/controller/CompensationController.java
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -6,24 +6,48 @@ import com.mindex.challenge.service.CompensationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sun.rmi.runtime.Log;
+
+import java.util.Collections;
 
 @RestController
+@RequestMapping(value = {"/compensation"})
 public class CompensationController {
     private static final Logger LOG = LoggerFactory.getLogger(CompensationController.class);
 
     @Autowired
     private CompensationService compensationService;
 
-    @PostMapping("/compensation")
-    public Compensation create(@RequestBody Compensation compensation) {
+    @PostMapping
+    public ResponseEntity create(@RequestBody Compensation compensation) {
         LOG.debug("Received compensation create request for [{}]", compensation);
-        return compensationService.create(compensation);
+        try {
+            Compensation createdCompensation = compensationService.create(compensation);
+            return new ResponseEntity<>(createdCompensation, HttpStatus.OK);
+        } catch (Exception e) {
+            // rare case of errors happening in database
+            LOG.debug("Fatal error in creating entry for in database");
+            return new ResponseEntity(Collections.singletonMap("Error:", "Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @GetMapping("/compensation/{id}")
-    public Compensation read(@PathVariable String id) {
+    @GetMapping("/{id}")
+    public ResponseEntity read(@PathVariable String id) {
         LOG.debug("Received compensation read request for id [{}]", id);
-        return compensationService.read(id);
+        Compensation compensation = null;
+        try {
+            compensation = compensationService.read(id);
+            if (compensation != null) {
+                return new ResponseEntity<>(compensation, HttpStatus.OK);
+            }
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(Collections.singletonMap("Error:", "User not found"), HttpStatus.NOT_FOUND);
+        }
+        // this should never happen as try/catch will handle the errors already
+        LOG.debug("Fatal error in reading from the database");
+        return new ResponseEntity(Collections.singletonMap("Error:", "Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -5,21 +5,36 @@ import com.mindex.challenge.service.ReportingStructureService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+
+import java.util.Collections;
 
 @RestController
+@RequestMapping(value = {"/reportingStructure"})
 public class ReportingStructureController {
     private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureController.class);
 
     @Autowired
     private ReportingStructureService reportingStructureService;
 
-    @GetMapping("reportingStructure/{id}")
-    public ReportingStructure read(@PathVariable String id) {
+    @GetMapping("/{id}")
+    public ResponseEntity read(@PathVariable String id) {
         LOG.debug("Received reporting structure read request for id [{}]", id);
-        return reportingStructureService.read(id);
+        ReportingStructure reportingStructure = null;
+        try {
+            reportingStructure = reportingStructureService.read(id);
+            if (reportingStructure != null) {
+                return new ResponseEntity<>(reportingStructure, HttpStatus.OK);
+            }
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(Collections.singletonMap("Error:", "User not found"), HttpStatus.NOT_FOUND);
+        }
+        // this should never happen as the errors should be handle in the try/catch already
+        LOG.debug("Fatal error in reading from database");
+        return new ResponseEntity(Collections.singletonMap("Error:", "Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
